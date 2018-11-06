9b5a20f6d24f048723ace0134b38ee27d08d9ea0
Austin Kellar <ajk@eduroam-169-233-252-74.ucsc.edu>
2018-11-06 15:36:13 -0800

    instead of isClicked, destroy the branch script

--- Assets/Scripts/BranchScript.cs
+++ Assets/Scripts/BranchScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BranchScript : MonoBehaviour {
     private Vector3 startScale;
     private Transform branchForm;
-    private bool isClicked;
 
     public BranchSpawner.Direction treeLocation;
     public float speed;
@@ -14,7 +13,6 @@ public class BranchScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        isClicked = true;
         branchForm = GetComponentInChildren<Transform>();
         branchForm.localScale = new Vector3(xScale, 1f, 1f);
 
@@ -29,7 +27,7 @@ public class BranchScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if ((branchForm.localScale.x < maxScale.x) && Input.GetMouseButton(0) && isClicked) {
+        if (branchForm.localScale.x < maxScale.x) {
             if (treeLocation == BranchSpawner.Direction.Left) {
                 branchForm.Translate(5f*speed, 0f, 0f);
             } else {
@@ -39,7 +37,7 @@ public class BranchScript : MonoBehaviour {
         }
 
         if (Input.GetMouseButtonUp(0)) {
-            isClicked = false;
+            Destroy(this);
         }
 	}
 }
