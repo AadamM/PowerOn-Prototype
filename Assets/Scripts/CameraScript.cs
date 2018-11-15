f9199fc6326bf172bedd50c63de1c6eaf74def1b
Aadam Rahat Mohammad <armohamm@crm.ucsc.edu>
2018-11-14 16:46:23 -0800

    Got a dynamic camera working (based off of the window model of camera design) and resized branch collider to make it smaller

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
-    private Vector2 _cameraOffset;
-    private float _playerStartingHeight;
+    private Vector3 _cameraStartingPosition;
+    private PlayerMovement _playerController;
 
     public GameObject player;
 
 	// Use this for initialization.
 	void Start () {
-        _playerStartingHeight = player.transform.position.y;
-        _cameraOffset = new Vector2(transform.position.x - player.transform.position.x, transform.position.y - player.transform.position.y);
+        _cameraStartingPosition = transform.position;
+        _playerController = player.GetComponent<PlayerMovement>();
 	}
 	
 	// Update is called once per frame.
-	void Update () {
-        transform.position = new Vector3(0, (player.transform.position.y-_playerStartingHeight), -10f);
+	void FixedUpdate () {
+        if (player.transform.position.y > (transform.position.y + (GetComponent<Camera>().orthographicSize / 4)))
+        {
+            transform.Translate(0f, _playerController.playerRigidbody.velocity.y * Time.fixedDeltaTime, 0f);
+            //transform.position = new Vector3(0, (player.transform.position.y - _playerStartingHeight), -10f);
+        }
+        else if(player.transform.position.y < (transform.position.y - (GetComponent<Camera>().orthographicSize / 4)) && (_playerController.playerRigidbody.velocity.y < 0))
+        {
+            transform.Translate(0f, _playerController.playerRigidbody.velocity.y * Time.fixedDeltaTime, 0f);
+        }
+
+        if (transform.position.y < _cameraStartingPosition.y)
+        {
+            transform.position = _cameraStartingPosition;
+        }
 	}
 }
