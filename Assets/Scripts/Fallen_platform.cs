58abc83c5947acd3bfe69ed4baddf3b3590b4983
javy1022 <javywu1022@gmail.com>
2018-11-18 17:30:48 -0800

    first commit

---
+++ Assets/Scripts/Fallen_platform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fallen_platform : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public string player_name = "Player";
+    public float time_before_fallen = 1.5f;
+    public float time_before_destroy = 1.5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == player_name)
+        {
+            Invoke("fallen_branch", time_before_fallen);
+        }
+    }
+
+    void fallen_branch()
+    {
+        rb.isKinematic = false;
+        rb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+        Invoke("destory_branch", time_before_destroy);
+        return;
+    }
+
+    void destory_branch()
+    {
+        Destroy(gameObject);
+        return;
+    }
+
+}
+
---
+++ Assets/Scripts/SapScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SapScript : MonoBehaviour 
+{
+    public float sapRefillPercentage;
+
+    private ForestController _controller;
+    private ForestController1 _controller1;
+    private ForestController2 _controller2;
+
+    private void Start() 
+    {
+        _controller = GameObject.Find("Forest Controller").GetComponent<ForestController>();
+        _controller1 = GameObject.Find("Forest Controller1").GetComponent<ForestController1>();
+        _controller2 = GameObject.Find("Forest Controller2").GetComponent<ForestController2>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        RefillSap();
+        Destroy(gameObject);
+    }
+
+    private void RefillSap() 
+    {
+        _controller.Sap += (_controller.maxSap * sapRefillPercentage);
+        _controller1.Sap += (_controller.maxSap * sapRefillPercentage);
+        _controller2.Sap += (_controller.maxSap * sapRefillPercentage);
+        if (_controller.Sap > _controller.maxSap) 
+        {
+            _controller.Sap = _controller.maxSap;
+        }
+
+        if (_controller1.Sap > _controller1.maxSap)
+        {
+            _controller1.Sap = _controller1.maxSap;
+        }
+
+        if (_controller2.Sap > _controller2.maxSap)
+        {
+            _controller2.Sap = _controller2.maxSap;
+        }
+    }
+}
